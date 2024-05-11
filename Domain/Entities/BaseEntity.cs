507dfae689641c4fe7d4a8eef6cd28156ec8ace3
mustafa adry <mustafa.adry2025@gmail.com>
2024-05-11 19:48:03 +0300

    Add project files.

---
+++ Domain/Entities/BaseEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class BaseEntity
+    {
+        [Key]
+        [Column("ID")]
+        public int ID { get; set; }
+
+        [Column("CreatedDateTime")]
+        public DateTime CreatedDateTime { get; set; }
+
+        [Column("UpdatedDateTime")]
+        public DateTime? UpdatedDateTime { get; set; }
+
+        [Column("CreatedBy")]
+        public string CreatedBy { get; set; }
+
+        [Column("UpdatedBy")]
+        public string UpdatedBy { get; set; }
+    }
+}
---
+++ Domain/Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImgUrl { get; set; }
+        public DateTime PublishDate { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
